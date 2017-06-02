f46600d2ddc554258f4818d3ed108be46d508da5
Mohammad Zahid Hussain <mozahu91@gmail.com>
2017-06-02 16:40:23 -0700

    Update 020 Merge Sorted Arrays.py

--- 020 Merge Sorted Arrays.py
+++ 020 Merge Sorted Arrays.py
@@ -1,2 +1,17 @@
 def merge(arr1,arr2):
-m= len(arr1)
+  m= len(arr1)
+  n= len(arr2)
+  end= m+n-1
+  arr1.extend(arr2)
+  
+  while l1>=0 and l2>=0:
+    if arr2[l2] > arr1[l1]:
+      arr1[end]= arr2[l2]
+      l2-=1
+    else:
+      arr1[end] = arr1[l1]
+      l1-=1
+    end -=1
+  
+  return arr1
+      
