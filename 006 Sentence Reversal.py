a510f732e71b50bd1874d06bd9f1a0e49f1160f8
Mohammad Zahid Hussain <mozahu91@gmail.com>
2017-06-02 15:11:29 -0700

    Create 006 Sentence Reversal.py

---
+++ 006 Sentence Reversal.py
@@ -0,0 +1,63 @@
+'''
+Problem
+Given a string of words, reverse all the words. For example:
+
+Given:
+
+'This is the best'
+
+Return:
+
+'best the is This'
+
+As part of this exercise you should remove all leading and trailing whitespace. So that inputs such as:
+
+'  space here'  and 'space here      '
+
+both become:
+
+'here space'
+'''
+
+def rev_word1(s):
+    return " ".join(reversed(s.split()))
+
+#Or
+
+def rev_word2(s):
+    return " ".join(s.split()[::-1])
+ #This is a naive approach, algorithmically it can solved by
+ 
+ 
+ def rev_word3(s):
+    """
+    Manually doing the splits on the spaces.
+    """
+    
+    words = []
+    length = len(s)
+    spaces = [' ']
+    
+    # Index Tracker
+    i = 0
+    
+    # While index is less than length of string
+    while i < length:
+        
+        # If element isn't a space
+        if s[i] not in spaces:
+            
+            # The word starts at this index
+            word_start = i
+            
+            while i < length and s[i] not in spaces:
+                
+                # Get index where word ends
+                i += 1
+            # Append that word to the list
+            words.append(s[word_start:i])
+        # Add to index
+        i += 1
+        
+    # Join the reversed words
+    return " ".join(reversed(words))
