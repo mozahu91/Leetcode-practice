181714df1d7d1e0b07f3ae5f76b535b7370638c6
Mohammad Zahid Hussain <mozahu91@gmail.com>
2017-06-03 14:45:33 -0700

    Update 032 Implement a Queue.py

--- 032 Implement a Queue.py
+++ 032 Implement a Queue.py
@@ -3,7 +3,7 @@ A queue is an ordered collection of items where the addition of new items happen
 
 The most recently added item in the queue must wait at the end of the collection. The item that has been in the collection the longest is at the front. This ordering principle is sometimes called FIFO, first-in first-out. It is also known as “first-come first-served.”
 
-The simplest example of a queue is the typical line that we all participate in from time to time. We wait in a line for a movie, we wait in the check-out line at a grocery store, and we wait in the cafeteria line. The first person in that line is also the first person to get serviced/helped.
+The simplest example of a queue is the typical line that we all participate in from time to time. We wait in a line for a movie, we wait in the check-out line at a grocery store, and we wait in the cafeteria line. The first person in that line is also the first person to get serviced/helped .
 """
 
 class queue():
