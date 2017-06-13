573d49f4742bc35ce5c2d91ce787695583623377
Mohammad Zahid Hussain <mozahu91@gmail.com>
2017-06-12 17:16:02 -0700

    Create 069 Sequential Search Unordered.py

---
+++ 069 Sequential Search Unordered.py
@@ -0,0 +1,16 @@
+def seq_search(arr, ele):
+    
+    pos = 0
+    found = False
+    
+    while pos < len(arr) and not found:
+        if arr[pos] == ele:
+            found = True
+        else:
+            pos +=1
+    
+    return (found, pos)
+    
+    
+    
+  
