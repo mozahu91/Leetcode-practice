bfa4e36b8eef1906a957d6f73629278f3ac29aa0
Mohammad Zahid Hussain <mozahu91@gmail.com>
2017-06-13 20:03:42 -0700

    Create 111 Trim a BST.py

---
+++ 111 Trim a BST.py
@@ -0,0 +1,22 @@
+"""
+Given the root of a binary search tree and 2 numbers min and max, trim the tree such that all the numbers in the new tree are between min and max (inclusive). The resulting tree should still be a valid binary search tree. So, if we get this tree as input:
+Simple solution is to do a Post Order Traversal.
+
+"""
+
+def trimBST(tree, minVal, maxVal): 
+    
+    if not tree: 
+        return 
+    
+    tree.left=trimBST(tree.left, minVal, maxVal) 
+    tree.right=trimBST(tree.right, minVal, maxVal) 
+    
+    if minVal<=tree.val<=maxVal: 
+        return tree 
+    
+    if tree.val<minVal: 
+        return tree.right 
+    
+    if tree.val>maxVal: 
+        return tree.left 
