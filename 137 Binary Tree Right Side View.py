031bef0fe7377cf27aa2f0c3de32e1a1d2f7d7ef
Mohammad Zahid Hussain <mozahu91@gmail.com>
2017-06-15 20:02:07 -0700

    Create 137 Binary Tree Right Side View.py

---
+++ 137 Binary Tree Right Side View.py
@@ -0,0 +1,39 @@
+"""
+
+Given a binary tree, imagine yourself standing on the right side of it, return the values of the nodes you can see ordered from top to bottom.
+
+For example:
+Given the following binary tree,
+   1            <---
+ /   \
+2     3         <---
+ \     \
+  5     4       <---
+You should return [1, 3, 4].
+
+"""
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def rightSideView(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        def collect(node, depth):
+            if node:
+                if depth ==len(view):
+                    view.append(node.val)
+                collect(node.right, depth+1)
+                collect(node.left, depth+1)
+        
+        view = []
+        collect(root,0)
+        return view
+        
