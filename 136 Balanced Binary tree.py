73382673669d1159b91b5cc4b548decfc78b12f1
Mohammad Zahid Hussain <mozahu91@gmail.com>
2017-06-15 19:50:52 -0700

    Create 136 Balanced Binary tree.py

---
+++ 136 Balanced Binary tree.py
@@ -0,0 +1,31 @@
+"""
+Given a binary tree, determine if it is height-balanced.
+
+For this problem, a height-balanced binary tree is defined as a binary tree in which the depth of the two subtrees of every node never differ by more than 1.
+
+"""
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def isBalanced(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        if not root:
+            return True
+        
+        return abs(self.getHeight(root.left) - self.getHeight(root.right)) <= 1 and self.isBalanced(root.left) and self.isBalanced(root.right)
+
+    def getHeight(self, root):
+        if not root:
+            return 0
+
+        return 1 + max(self.getHeight(root.left), self.getHeight(root.right))
+        
