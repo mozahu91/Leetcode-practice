f46a90813602cc2d22fcdc6af35a683c2a789504
Mohammad Zahid Hussain <mozahu91@gmail.com>
2017-06-25 00:58:05 -0700

    Create 168 Maximum consecutive ones.py

---
+++ 168 Maximum consecutive ones.py
@@ -0,0 +1,11 @@
+def findMaxConsecutiveOnes(nums):
+        cnt = 0
+        ans = 0
+        for num in nums:
+            if num == 1:
+                cnt += 1
+                ans = max(ans, cnt)
+            else:
+                cnt = 0
+        return ans
+    
