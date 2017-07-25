66bd879e4f7c1838085071e2154116e303e6e124
Mohammad Zahid Hussain <mozahu91@gmail.com>
2017-07-25 00:40:09 -0700

    Create 175 LinkedList Palindrome.py

---
+++ 175 LinkedList Palindrome.py
@@ -0,0 +1,20 @@
+def isPalindrome(self, head):
+    fast = slow = head
+    # find the mid node
+    while fast and fast.next:
+        fast = fast.next.next
+        slow = slow.next
+    # reverse the second half
+    node = None
+    while slow:
+        nxt = slow.next
+        slow.next = node
+        node = slow
+        slow = nxt
+    # compare the first and second half nodes
+    while node: # while node and head:
+        if node.val != head.val:
+            return False
+        node = node.next
+        head = head.next
+    return True
