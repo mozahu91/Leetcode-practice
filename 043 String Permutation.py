d7b120f0991c851407b587bd1c85c94c1e63b35c
Mohammad Zahid Hussain <mozahu91@gmail.com>
2018-01-27 17:18:45 -0800

    Update 043 String Permutation.py

--- 043 String Permutation.py
+++ 043 String Permutation.py
@@ -1,13 +1,14 @@
+"""
 String Permutation
 Problem Statement
 Given a string, write a function that uses recursion to output a list of all the possible permutations of that string.
 
 For example, given s='abc' the function should return ['abc', 'acb', 'bac', 'bca', 'cab', 'cba']
 
-Note: If a character is repeated, treat each occurence as distinct, for example an input of 'xxx' would return a list with 6 "versions" of 'xxx'
+Note: If a character is repeated, treat each occurence as distinct, for example an input of 'xxx' would return a list
+with 6 "versions" of 'xxx'
 
-Fill Out Your Solution Below"""
-
+Fill Out Your Solution Below
 
 """
 
