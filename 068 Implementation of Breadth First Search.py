57eb1deceba2b22121c1f22e689ac00a44e4e190
Mohammad Zahid Hussain <mozahu91@gmail.com>
2018-02-06 10:33:52 -0800

    Update 068 Implementation of Breadth First Search.py

--- 068 Implementation of Breadth First Search.py
+++ 068 Implementation of Breadth First Search.py
@@ -1,5 +1,11 @@
 """
-An alternative algorithm called Breath-First search provides us with the ability to return the same results as DFS but with the added guarantee to return the shortest-path first. This algorithm is a little more tricky to implement in a recursive manner instead using the queue data-structure, as such I will only being documenting the iterative approach. The actions performed per each explored vertex are the same as the depth-first implementation, however, replacing the stack with a queue will instead explore the breadth of a vertex depth before moving on. This behavior guarantees that the first path located is one of the shortest-paths present, based on number of edges being the cost factor.
+An alternative algorithm called Breath-First search provides us with the ability to return the same results as DFS 
+but with the added guarantee to return the shortest-path first. 
+This algorithm is a little more tricky to implement in a recursive manner instead using the queue data-structure, 
+as such I will only being documenting the iterative approach. The actions performed per each explored vertex are the same 
+as the depth-first implementation, however, replacing the stack with a queue will instead explore the breadth of a vertex 
+depth before moving on. This behavior guarantees that the first path located is one of the shortest-paths present, based
+on number of edges being the cost factor.
 """
 
 
