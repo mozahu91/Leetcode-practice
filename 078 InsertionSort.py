36a68e9aafd302c393a82ccea3ad560c01a60b8f
Mohammad Zahid Hussain <mozahu91@gmail.com>
2018-03-16 09:15:28 -0700

    Update 078 InsertionSort.py

--- 078 InsertionSort.py
+++ 078 InsertionSort.py
@@ -1,6 +1,9 @@
 """
-Insertion sort is a simple sorting algorithm that builds the final sorted array (or list) one item at a time. It is much less efficient on large lists than more advanced algorithms such as quicksort, heapsort, or merge sort. However, insertion sort provides several advantages:
-Simple implementation: Efficient for (quite) small data sets, much like other quadratic sorting algorithms More efficient in practice than most other simple quadratic (i.e., O(n2)) algorithms such as, selection sort or bubble sort
+Insertion sort is a simple sorting algorithm that builds the final sorted array (or list) one item at a time. 
+It is much less efficient on large lists than more advanced algorithms such as quicksort, heapsort, or merge sort. 
+However, insertion sort provides several advantages:
+Simple implementation: Efficient for (quite) small data sets, much like other quadratic sorting algorithms 
+More efficient in practice than most other simple quadratic (i.e., O(n2)) algorithms such as, selection sort or bubble sort
 Stable, Method: Insertion
 """
 
